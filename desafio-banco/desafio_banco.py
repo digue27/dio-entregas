46678e01fff0364157256cc6435562bac3b0ea1e
Ednaldo dos Santos Costa <ednaldodigue27@gmail.com>
2024-05-15 21:02:24 -0300

    V1.2

--- desafio-banco/desafio_banco.py
+++ desafio-banco/desafio_banco.py
@@ -10,58 +10,60 @@ saldo = 0.0
 deposito_total = 0.0
 saque_total = 0.0
 houve_movimentacao = False
-QUANTIDADE_DE_SAQUE = 3
-
+LIMITE_DE_SAQUE = 3
+quantidade_de_saque = 0
 while True :
 
-    opcao = input("O que deseja fazer?\n")
-    opcao = opcao.lower()
     print(menu)
+    opcao = input("\nO que deseja fazer?")
+    opcao = opcao.lower()
+
     
     if(opcao == "d"):
 
-        movimento_de_caixa = float(input("Valor do Deposito: \n"))
+        movimento_de_caixa = float(input("\nValor do Deposito: "))
 
         if(movimento_de_caixa > 0):
             deposito_total += movimento_de_caixa
             saldo = saldo + movimento_de_caixa
             houve_movimentacao = True
 
-            print(f"Deposito de R$: {movimento_de_caixa}")
+            print(f"\nDeposito de R$:{movimento_de_caixa} REALIZADO.")
 
         else:
-            print("Não foi possivel Depositar")
+            print("NÃO FOI POSSÍVEL DEPOSITAR.")
 
     elif(opcao == 's'):
 
-        if(QUANTIDADE_DE_SAQUE > 0 and saldo >= 0):
+        if(quantidade_de_saque < LIMITE_DE_SAQUE):
 
-            movimento_de_caixa = float(input("Valor do Saque: \n"))
+            movimento_de_caixa = float(input("\nValor do Saque: "))
 
             if(movimento_de_caixa <= 500 and movimento_de_caixa <= saldo):
                 saque_total = saque_total + movimento_de_caixa
                 saldo = saldo - movimento_de_caixa
-                QUANTIDADE_DE_SAQUE -= 1
+                quantidade_de_saque += 1
                 houve_movimentacao = True
 
-                print(f"Saque de R$: {movimento_de_caixa}")
+                print(f"\nSaque de R$:{movimento_de_caixa} REALIZADO.")
             else:
-                print("Não foi possivel sacar.")
+                print("NÃO FOI POSSÍVEL SACAR.")
 
     elif(opcao == "e"):
 
         if(houve_movimentacao == True):
-            print("Extrato")
+            print("")
             print(f'''
+                            Extrato
                     Valor total de Saque: R$:{saque_total},
                     Valor total de Deposito: R$:{deposito_total},
-                    O Saldo é R$:{saldo}.''')
+                        O Saldo é R$:{saldo}.''')
             
         else:
-            print("Não houve movimentações.")
+            print("NÃO HOUVE MOVIMENTAÇÕES.")
 
     elif(opcao == "q"):
         break
 
     else:
-        print("Opção informada não valida")
+        print("OPÇÃO INFORMADA INVÁLIDA.")
